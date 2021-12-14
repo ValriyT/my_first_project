ad97afd006fe9b4bdae9edfd65550c0cb68d08db
ValeriyT <valerka2009@gmail.com>
2021-12-14 17:35:32 +0300

    initialization commit

--- FirstApp.java
+++ FirstApp.java
@@ -5,7 +5,7 @@ public class FirstApp {
        // System.out.println ("Абракадабра777") ;
         int a;
         int b;
-        a = 128;
+        a = 148;
 
         System.out.println("a=" +a);
         b = a/2;
